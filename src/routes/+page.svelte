39ccdcb06094519f88e973688ec4152fbd82976a
Michael Fouquet <mfouquet@users.noreply.github.com>
2023-09-18 11:52:10 -0500

    :sparkles: Bring in TeamSync page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,427 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+  import bulletpoint from "$lib/images/BulletPoint.svg";
+  import ellipse10 from "$lib/images/Ellipse-10.png";
+  import ellipse101 from "$lib/images/Ellipse-10_1.png";
+  import ellipse102 from "$lib/images/Ellipse-10_2.png";
+  import illustration11 from "$lib/images/Illustration-1-1.png";
+  import illustration1 from "$lib/images/Illustration-1.png";
+  import illustration21 from "$lib/images/Illustration-2-1.png";
+  import illustration31 from "$lib/images/Illustration-3-1.png";
+  import image from "$lib/images/Image.png";
+  import image1 from "$lib/images/Image_1.png";
+  import image2 from "$lib/images/Image_2.png";
+  import image3 from "$lib/images/Image_3.png";
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<div class="content">
+  <div class="headline" />
+</div>
+<main id="Hero" class="frame-13874">
+  <img
+    src={image3}
+    loading="lazy"
+    width="764"
+    height="634"
+    alt="This is a 📷 of two people talking"
+    class="image-1"
+  />
+  <div class="content-2">
+    <div class="headline">
+      <h1 class="heading">Supercharge Team Work</h1>
+      <div class="body">
+        With our project management, communication, and file sharing tools, you
+        can work together seamlessly from anywhere in the world.
+      </div>
+    </div>
+    <div class="buttons">
+      <a href="#" class="button w-button">Try TeamSync</a>
+      <a href="#" class="button-2 w-button">Request a demo</a>
+    </div>
+  </div>
+</main>
+<section id="Value-prop-1" class="value-prop-1-2">
+  <img
+    src={illustration1}
+    loading="lazy"
+    width="528"
+    height="423.5"
+    alt="Pieces of pieces user interface, pieces of pieces, pieces of pieces"
+    class="illustration-2"
+  />
+  <div class="content-6">
+    <h1 class="heading-2">
+      Streamline your work processes &amp; boost productivity
+    </h1>
+    <div class="body">
+      With features like project management, task delegation, collaboration, and
+      real-time notifications, you&#x27;ll have everything you need to stay on
+      top of your work.
+    </div>
+  </div>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<section id="Value-prop-2" class="value-prop-2">
+  <div class="content-3">
+    <div class="heading-2">Access to your team on the go</div>
+    <div class="body">
+      Communicate in real-time with team members through our chat, video, and
+      voice call features. Keep everyone up-to-date on project progress and
+      updates.
+    </div>
+  </div>
+  <img
+    src={illustration11}
+    loading="lazy"
+    width="564.5"
+    height="713.5"
+    alt="Picture of an iPhone showing Teamsync"
+    class="illustration-1-1"
+  />
+</section>
+<section id="Pricing" class="pricing-2">
+  <div class="pricing-3">
+    <div class="headline-10">
+      <h2 class="heading-3">Pricing</h2>
+      <div class="body-3">
+        Find a plan that fits all of your needs and more.
+      </div>
+    </div>
+    <div class="frame-13900">
+      <div class="basic-3">
+        <div class="frame-13901">
+          <div class="tier-name-7">
+            <div class="text-14">Enterprise</div>
+            <div class="text-15">Reach out</div>
+          </div>
+          <div class="bullets-6">
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-3">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Access to all of Pro features</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Premium support</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Access to early bird feature releases</div>
+            </div>
+          </div>
+        </div>
+        <div class="tier-basic-3">
+          <a href="#" class="button-3 w-button">Download</a>
+        </div>
+      </div>
+      <div class="pro-4">
+        <div class="frame-13902">
+          <div class="tier-name-7">
+            <div class="text-14">Enterprise</div>
+            <div class="text-15">Reach out</div>
+          </div>
+          <div class="bullets-6">
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-3">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Limited to 10 teammates</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Unlimited teams</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Unlimited storage space</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Customized integrations</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Access to advanced analytics</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Access to beta features</div>
+            </div>
+          </div>
+        </div>
+        <div class="tier-basic-3">
+          <a href="#" class="button-4 w-button">Subscribe for $20/month</a>
+        </div>
+      </div>
+      <div class="basic-3">
+        <div class="frame-13901">
+          <div class="tier-name-7">
+            <div class="text-14">Enterprise</div>
+            <div class="text-15">Reach out</div>
+          </div>
+          <div class="bullets-6">
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-3">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Access to all of Pro features</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Premium support</div>
+            </div>
+            <div class="bullets-7">
+              <div class="bullet-point-wrapper-4">
+                <img
+                  src={bulletpoint}
+                  loading="lazy"
+                  width="8"
+                  height="8"
+                  alt=""
+                  class="vectors-wrapper-11"
+                />
+              </div>
+              <div class="feature-2">Access to early bird feature releases</div>
+            </div>
+          </div>
+        </div>
+        <div class="tier-basic-3" />
+        <a href="#" class="button-3 w-button">Reach out</a>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="Value-prop-3" class="frame-13877">
+  <img
+    src={illustration21}
+    loading="lazy"
+    width="738"
+    height="586.5"
+    alt="Pieces of the team sync user interface. One large calendar with meetings with chat bubbles and avatars around it"
+    class="illustration-2-1"
+  />
+  <div class="content-3">
+    <div class="heading-2">
+      Easily manage workflows for projects of any size
+    </div>
+    <div class="body">
+      Stay organized and on track with our powerful project management tools.
+      Assign tasks, set deadlines, and track progress in real-time.
+    </div>
+  </div>
+</section>
+<section id="Articles" class="articles">
+  <div class="articles-2">
+    <div class="headline-6">Latest articles</div>
+    <div class="articles-3">
+      <div class="article">
+        <img
+          src={image}
+          loading="lazy"
+          width="300"
+          height="209"
+          alt="People in an office working. One person is pointing at a monitor while the others watch them."
+          class="image"
+        />
+        <div class="content-4">
+          <div class="copy">
+            <div class="headline-7">
+              <div class="text-2">Time managament</div>
+              <div class="text-3">Jan 22, 2023</div>
+            </div>
+            <div class="body-2">
+              Mastering time management: Tips and strategies for a more...
+            </div>
+          </div>
+          <div class="author">
+            <img
+              src={ellipse10}
+              loading="lazy"
+              width="44"
+              height="44"
+              alt=""
+              class="ellipse-10"
+            />
+            <div class="text-4">Kate Vesa</div>
+          </div>
+        </div>
+      </div>
+      <div class="article">
+        <img
+          src={image1}
+          loading="lazy"
+          width="300"
+          height="209"
+          alt="Two people looking at a computer while sitting and smiling at each other."
+          class="image"
+        />
+        <div class="content-5">
+          <div class="copy">
+            <div class="headline-7">
+              <div class="text-2">Collaboration</div>
+              <div class="text-3">Feb 10, 2023</div>
+            </div>
+            <div class="body-2">
+              The Power of Collaboration: Why Working Together is Key to
+              Success...
+            </div>
+          </div>
+          <div class="author">
+            <img
+              src={ellipse101}
+              loading="lazy"
+              width="44"
+              height="44"
+              alt=""
+              class="ellipse-10"
+            />
+            <div class="text-4">Tony Wilson</div>
+          </div>
+        </div>
+      </div>
+      <div class="article">
+        <img
+          src={image2}
+          loading="lazy"
+          width="300"
+          height="209"
+          alt="On person looking at a computer screen with another person on it. They are in a web call."
+          class="image"
+        />
+        <div class="content-5">
+          <div class="copy">
+            <div class="headline-7">
+              <div class="text-2">Meetings</div>
+              <div class="text-3">March 1, 2023</div>
+            </div>
+            <div class="body-2">
+              Maximizing Productivity: How to Make Your Meetings More
+              Effective...
+            </div>
+          </div>
+          <div class="author">
+            <img
+              src={ellipse102}
+              loading="lazy"
+              width="44"
+              height="44"
+              alt=""
+              class="ellipse-10"
+            />
+            <div class="text-4">Rita Verna</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section id="Value-prop-4" class="value-prop-4">
+  <div class="content-3">
+    <div class="heading-2">Stay on track and never miss a beat</div>
+    <div class="body">
+      Get instant alerts and notifications about upcoming deadlines. Our chat
+      feature allows you to quickly communicate with your team and get the
+      support you need to meet all project requirements.
+    </div>
+  </div>
+  <img
+    src={illustration31}
+    loading="lazy"
+    width="516"
+    height="388.5"
+    alt="A conversation between two teammates about scheduling a meeting"
+    class="illustration-3-1"
+  />
+</section>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import facebookLogo from "$lib/images/facebook.svg";
+  import instagramLogo from "$lib/images/instagram.svg";
+  import twitterLogo from "$lib/images/twitter.svg";
+  import whatsappLogo from "$lib/images/whatsapp.svg";
+</script>
+
+<footer id="Footer" class="footer-2">
+  <div class="frame-13896">
+    <div class="frame-13895">
+      <div class="logo-2">TeamSync</div>
+      <div class="frame-13897">
+        <img
+          src={instagramLogo}
+          loading="lazy"
+          width="29.01435661315918"
+          height="29.01435661315918"
+          alt="Instagram logo"
+          class="vectors-wrapper-10"
+        />
+        <img
+          src={whatsappLogo}
+          loading="lazy"
+          width="29.01435661315918"
+          height="29.01435661315918"
+          alt="Whatsapp logo"
+          class="vectors-wrapper-10"
+        />
+        <img
+          src={twitterLogo}
+          loading="lazy"
+          width="29.01435661315918"
+          height="29.01435661315918"
+          alt="Twitter logo"
+          class="vectors-wrapper-10"
+        />
+        <img
+          src={facebookLogo}
+          loading="lazy"
+          width="29.01435661315918"
+          height="29.01435661315918"
+          alt="Facebook logo"
+          class="vectors-wrapper-10"
+        />
+      </div>
+    </div>
+    <div class="frame-13898">
+      <a href="#" class="link-6">Features</a>
+      <a href="#" class="link-7">Resources</a>
+      <a href="#" class="link-7">Articles</a>
+    </div>
+    <div class="frame-13898">
+      <a href="#" class="link-7">FAQs</a>
+      <a href="#" class="link-7">Blogs</a>
+      <a href="#" class="link-7">Support</a>
+    </div>
+    <div class="frame-13899">
+      <a href="#" class="link-8">Careers</a>
+      <a href="#" class="link-8">Terms of Service</a>
+      <a href="#" class="link-8">Privacy Policy</a>
+    </div>
+  </div>
+</footer>
